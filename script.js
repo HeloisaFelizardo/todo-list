a1f7981dff87de55c7aae325e5c59ca79aaeb55d
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-11-22 19:04:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const tarefa = document.querySelector('#tarefa');
+const btn = document.querySelector('#btn');
+const lista = document.querySelector('#lista');
+
+btn.addEventListener('click', function () {
+	if (tarefa.value == '') {
+		alert('Digite alguma tarefa');
+	} else {
+		lista.innerHTML += `<li>
+            <i class="fa fa-check-circle check"></i>
+            <span>${tarefa.value}</span>
+            <i class="fa fa-trash-can close"></i>
+        </li>`;
+		tarefa.value = '';
+
+		// Adiciona o event listener para o novo item criado
+		const close = document.querySelector('.close');
+		close.addEventListener('click', function (e) {
+			e.target.parentElement.remove();
+		});
+	}
+});
+
+// Event delegation para elementos com classe 'close'
+lista.addEventListener('click', function (e) {
+	if (e.target.classList.contains('close')) {
+		e.target.parentElement.remove();
+	} else if (e.target.classList.contains('check')) {
+		e.target.style.color = '#349223';
+		e.target.nextElementSibling.style.textDecoration = 'line-through';
+	}
+});
